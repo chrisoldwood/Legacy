16508b273a53f4206dcf89989932732c0b4cbf1e
Admin <admin@chrisoldwood.com>
2013-02-06 17:08:51 +0000

    Fixed compilation warnings with GCC 4.7.1.

--- TArray.hpp
+++ TArray.hpp
@@ -298,19 +298,19 @@ template<class T> inline void TPtrArray<T>::DeleteAll()
 
 template<class T> inline void TPtrArray<T>::ShallowCopy(const TPtrArray<T>& oRHS)
 {
-	Reserve(oRHS.Size());
+	this->Reserve(oRHS.Size());
 
 	for (size_t i = 0; i < oRHS.Size(); ++i)
-		Add(oRHS.At(i));
+		this->Add(oRHS.At(i));
 }
 
 template<class T> inline void TPtrArray<T>::DeepCopy(const TPtrArray<T>& oRHS)
 {
-	Reserve(oRHS.Size());
+	this->Reserve(oRHS.Size());
 
 	for (size_t i = 0; i < oRHS.Size(); ++i)
 	{
-		Add(new T(*oRHS.At(i)));
+		this->Add(new T(*oRHS.At(i)));
 	}
 }
 
